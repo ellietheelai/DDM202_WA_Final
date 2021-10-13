2dcfadbb4bce79eb3d988a8a313532b681e016ff
Ellie Wong <elliewong1125@gmail.com>
2021-10-13 23:52:05 +0800

    Created customer table

---
+++ customer.php
@@ -0,0 +1,74 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- container -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Create Customer</h1>
+        </div>
+        <!-- html form to create product will be here -->
+        <!-- PHP insert code will be here -->
+        <?php
+        if ($_POST) {
+            // include database connection
+            include 'config/database.php';
+            try {
+                // insert query
+                $query = "INSERT INTO customers SET customer_name=:customer_name, birthdate=:birthdate";
+                // prepare query for execution
+                $stmt = $con->prepare($query);
+                // posted values
+                $c_name = htmlspecialchars(strip_tags($_POST['customer_name']));
+                // bind the parameters
+                $stmt->bindParam(':customer_name', $c_name);
+                // specify when this record was inserted to the database
+                $dob = date('Y-m-d H:i:s');
+                $stmt->bindParam(':birthdate', $dob);
+                // Execute the query
+                if ($stmt->execute()) {
+                    echo "<div class='alert alert-success'>Record was saved.</div>";
+                } else {
+                    echo "<div class='alert alert-danger'>Unable to save record.</div>";
+                }
+            }
+            // show error
+            catch (PDOException $exception) {
+                die('ERROR: ' . $exception->getMessage());
+            }
+        }
+        ?>
+
+
+        <!-- html form here where the product information will be entered -->
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <table class='table table-hover table-responsive table-bordered'>
+                <tr>
+                    <td>Customer Name</td>
+                    <td><input type='text' name='customer_name' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Date of Birth</td>
+                    <td><input type='date' name='date of birth' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type='submit' value='Save' class='btn btn-primary' />
+                        <a href='index.php' class='btn btn-danger'>Back to customer details</a>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+    <!-- end .container -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>

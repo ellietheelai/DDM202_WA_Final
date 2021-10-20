7c8519d42cd61dfa9e41df0fc3c7fd908ac7e9be
Ellie Wong <elliewong1125@gmail.com>
2021-10-21 06:03:40 +0800

    Did changes to customer index/read

--- customer_index.php
+++ customer_index.php
@@ -11,7 +11,7 @@
     <!-- container -->
     <div class="container">
         <div class="page-header">
-            <h1>Read Products</h1>
+            <h1>Read Customers</h1>
         </div>
 
         <!-- PHP code to read records will be here -->
@@ -22,7 +22,7 @@
         // delete message prompt will be here
 
         // select all data
-        $query = "SELECT id, name, description, price FROM products ORDER BY id DESC";
+        $query = "SELECT customer_id, customer_name, birthdate FROM customers ORDER BY customer_id DESC";
         $stmt = $con->prepare($query);
         $stmt->execute();
 
@@ -30,7 +30,7 @@
         $num = $stmt->rowCount();
 
         // link to create record form
-        echo "<a href='create.php' class='btn btn-primary m-b-1em'>Create New Product</a>";
+        echo "<a href='customer.php' class='btn btn-primary m-b-1em'>Create New Customer</a>";
 
         //check if more than 0 record found
         if ($num > 0) {
@@ -40,10 +40,9 @@
 
             //creating our table heading
             echo "<tr>";
-            echo "<th>ID</th>";
-            echo "<th>Name</th>";
-            echo "<th>Description</th>";
-            echo "<th>Price</th>";
+            echo "<th>Customer ID</th>";
+            echo "<th>Customer Name</th>";
+            echo "<th>Birthdate</th>";
             echo "<th>Action</th>";
             echo "</tr>";
 
@@ -55,19 +54,18 @@
                 extract($row);
                 // creating new table row per record
                 echo "<tr>";
-                echo "<td>{$id}</td>";
-                echo "<td>{$name}</td>";
-                echo "<td>{$description}</td>";
-                echo "<td>{$price}</td>";
+                echo "<td>{$customer_id}</td>";
+                echo "<td>{$customer_name}</td>";
+                echo "<td>{$birthdate}</td>";;
                 echo "<td>";
                 // read one record
-                echo "<a href='read_one.php?id={$id}' class='btn btn-info m-r-1em'>Read</a>";
+                echo "<a href='customer_read.php?id={$customer_id}' class='btn btn-info m-r-1em'>Read</a>";
 
                 // we will use this links on next part of this post
-                echo "<a href='update.php?id={$id}' class='btn btn-primary m-r-1em'>Edit</a>";
+                echo "<a href='update.php?id={$customer_id}' class='btn btn-primary m-r-1em'>Edit</a>";
 
                 // we will use this links on next part of this post
-                echo "<a href='#' onclick='delete_user({$id});'  class='btn btn-danger'>Delete</a>";
+                echo "<a href='#' onclick='delete_user({$customer_id});'  class='btn btn-danger'>Delete</a>";
                 echo "</td>";
                 echo "</tr>";
             }
--- customer_read.php
+++ customer_read.php
@@ -15,14 +15,14 @@
     <!-- container -->
     <div class="container">
         <div class="page-header">
-            <h1>Read Product</h1>
+            <h1>Read Customers</h1>
         </div>
 
         <!-- PHP read one record will be here -->
         <?php
         // get passed parameter value, in this case, the record ID
         // isset() is a PHP function used to verify if a value is there or not
-        $id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
+        $customer_id = isset($_GET['customer_id']) ? $_GET['customer_id'] : die('ERROR: Record ID not found.');
 
         //include database connection
         include 'config/database.php';
@@ -30,11 +30,11 @@
         // read current record's data
         try {
             // prepare select query
-            $query = "SELECT id, name, description, price FROM products WHERE id = ? LIMIT 0,1";
+            $query = "SELECT customer_id, customer_name, birthdate FROM customers WHERE customer_id = ? LIMIT 0,1";
             $stmt = $con->prepare($query);
 
             // this is the first question mark
-            $stmt->bindParam(1, $id);
+            $stmt->bindParam(1, $customer_id);
 
             // execute our query
             $stmt->execute();
@@ -43,9 +43,8 @@
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
             // values to fill up our form
-            $name = $row['name'];
-            $description = $row['description'];
-            $price = $row['price'];
+            $customer_name = $row['customer_name'];
+            $birthdate = $row['birthdate'];
         }
 
         // show error
@@ -60,21 +59,18 @@
         <!--we have our html table here where the record will be displayed-->
         <table class='table table-hover table-responsive table-bordered'>
             <tr>
-                <td>Name</td>
-                <td><?php echo htmlspecialchars($name, ENT_QUOTES);  ?></td>
+                <td>Customer Name</td>
+                <td><?php echo htmlspecialchars($customer_name, ENT_QUOTES);  ?></td>
             </tr>
             <tr>
-                <td>Description</td>
-                <td><?php echo htmlspecialchars($description, ENT_QUOTES);  ?></td>
+                <td>Birthdate</td>
+                <td><?php echo date($birthdate, ENT_QUOTES);  ?></td>
             </tr>
-            <tr>
-                <td>Price</td>
-                <td><?php echo htmlspecialchars($price, ENT_QUOTES);  ?></td>
-            </tr>
+            
             <tr>
                 <td></td>
                 <td>
-                    <a href='index.php' class='btn btn-danger'>Back to read products</a>
+                    <a href='customer_index.php' class='btn btn-danger'>Back to read customers</a>
                 </td>
             </tr>
         </table>

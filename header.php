139274d801cbd302947d1367d262da9b79ec53e1
Ellie Wong <elliewong1125@gmail.com>
2021-12-27 13:09:06 +0800

    header changed position

--- header.php
+++ header.php
@@ -9,41 +9,52 @@
     <title>Bootstrap Exercise</title>
     </head>
     <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid px-4">
-            <a class="navbar-brand" href="welcome.php">Online Store</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-sm navbar-light bg-light" aria-label="Third navbar example">
+        <div class="container-fluid" bis_skin_checked="1">
+            <a class="navbar-brand" href="#">Online Store</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03"
+                aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarText">
 
-                <div class="dropdown ms-auto ">
-                    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"> Products </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                        <li><a class="dropdown-item" href="products_create.php">Create Product</a></li>
-                        <li><a class="dropdown-item" href="products_index.php">Read Product</a></li>
-                    </ul>
-                    
-                    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false"> Customers </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButtons">
-                        <li><a class="dropdown-item" href="customer_create.php">Create Customer</a></li>
-                        <li><a class="dropdown-item" href="customer_index.php">Read Customer</a></li>
-                    </ul>
-
-                    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false"> Category </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButtons">
-                        <li><a class="dropdown-item" href="category_create.php">Create Category</a></li>
-                        <li><a class="dropdown-item" href="category_index.php">Read Category</a></li>
-                    </ul>
-
-                    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false"> Order </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButtons">
-                        <li><a class="dropdown-item" href="order_create.php">Create Order</a></li>
-                        <li><a class="dropdown-item" href="order_index.php">Read Order</a></li>
-                    </ul>
-                </div>
-                <a class="nav-link" href="logout.php">Log Out</a>
-    
+            <div class="collapse navbar-collapse" id="navbarsExample03" bis_skin_checked="1">
+                <ul class="navbar-nav ms-auto px-5 mb-2 mb-sm-0">
+                    <li class="nav-item dropdown px-3">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown"
+                            aria-expanded="false">Products</a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                            <li><a class="dropdown-item" href="products_create.php">Create Product</a></li>
+                            <li><a class="dropdown-item" href="products_index.php">Read Product</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown"
+                            aria-expanded="false">Category</a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                            <li><a class="dropdown-item" href="category_create.php">Create Category</a></li>
+                            <li><a class="dropdown-item" href="category_index.php">Read Category</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown"
+                            aria-expanded="false">Customers</a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                            <li><a class="dropdown-item" href="customer_create.php">Create Customer</a></li>
+                            <li><a class="dropdown-item" href="customer_index.php">Read Customer</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown px-3">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown"
+                            aria-expanded="false">Orders</a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                            <li><a class="dropdown-item" href="order_create.php">Create Order</a></li>
+                            <li><a class="dropdown-item" href="order_index.php">Read Order</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                    <a class="nav-link" href="logout.php">Logout</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
